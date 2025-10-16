02b33e0e5065a266a6c09057f31d16412662a451
Ny3usi <akokojeremy8@gmail.com>
2025-10-16 20:18:37 +0300

    Initial commit: project structure and main file

---
+++ lms/main.py
@@ -0,0 +1,9 @@
+# main.py
+print("Welcome to EduHub LMS")
+
+# EduHub-LMS manages lecturers, students, classes, and learning resources.
+# Key goals:
+# - Simplify class management
+# - Track academic progress
+# - Organize resources efficiently
+# - Enhance team collaboration
